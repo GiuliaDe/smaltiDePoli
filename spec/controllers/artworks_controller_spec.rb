454cf5db4f57e51a824b363872a562490adb354b
GiuliaDep <giulia.depoli@wattics.com>
2016-07-09 12:26:07 +0200

    Add test structure to artworks controller

---
+++ spec/controllers/artworks_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ArtworksController do
+  describe "GET #index" do
+    it "populates an array of artworks"
+    it "renders the :index view"
+  end
+
+  describe "GET #show" do
+    it "assigns the requested contact to @artwork"
+    it "renders the :show template"
+  end
+
+  end
+end
